dd77f0c1c70f348915e74a1bb09f08b70965fe88
jayeshyadav31 <yjayesh60@gmail.com>
2024-10-16 01:10:06 +0530

    addded card component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import './Card.css'
+
+export default function Card(props) {
+  return (
+    <>
+        <div className="card-container">
+            <div className="card-id-wrapper">
+                <div className="card-id">{props.cardDetails.id}</div>
+                <div className="card-profile">
+                    <div className="card-profile-initial">{props.cardDetails.userObj.name[0]}{props.cardDetails.userObj.name[1]}</div>
+                    <div className={props.cardDetails.userObj.available ?"card-profile-initial-available card-profile-initial-available-true" : "card-profile-initial-available"}></div>
+                </div>
+            </div>
+            <div className="card-title">
+                {props.cardDetails.title}
+            </div>
+            <div className="card-tag">
+                {
+                    {
+                        0: <div className="card-tag-icon"><img src='/assets/No-priority.svg'/></div>,
+                        1: <div className="card-tag-icon"><img src='/assets/Img - Low Priority.svg'/></div>,
+                        2: <div className="card-tag-icon"><img src='/assets/Img - Medium Priority.svg'/></div>,
+                        3: <div className="card-tag-icon"><img src='/assets/Img - High Priority.svg'/></div>,
+                        4: <div className="card-tag-icon"><img src='/assets/SVG - Urgent Priority colour.svg'/></div>
+                    }[props.cardDetails.priority]
+                }
+
+                {
+                    props.cardDetails.tag.map((tag) => {
+                        return(
+                            <div className="card-tag-box">
+                                <div className="card-tag-title">{tag}</div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    </>
+  )
+}
