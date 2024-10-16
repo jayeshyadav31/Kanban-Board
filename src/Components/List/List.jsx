7343b16bba7561745b886456bca0de01177ab05b
jayeshyadav31 <yjayesh60@gmail.com>
2024-10-16 08:59:46 +0530

    added List Component

---
+++ src/Components/List/List.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+
+import './List.css'
+import Card from '../Card/Card'
+
+let cardCount = 0;
+
+export default function List(props) {
+  return (
+    <>
+        <div className="list-container">
+            <div className="list-header">
+                <div className="list-header-left">
+                    {
+                        {
+                            'status' : <>{
+                                {
+                                    'Backlog': <div className="list-icon">
+                                    <img src='/assets/Backlog.svg'></img>
+                                    </div>,
+                                    'Todo': <div className="list-icon">
+                                    <img src='/assets/To-do.svg'></img>
+                                    </div>,
+                                    'In progress': <div className="list-icon">
+                                    <img src='/assets/in-progress.svg'></img>
+                                    </div>,
+                                    'Done': <div className="list-icon">
+                                    <img src='/assets/Done.svg'></img>
+                                    </div>,
+                                    'Cancelled': <div className="list-icon">
+                                    <img src='/assets/Cancelled.svg'></img>
+                                    </div>
+                                }[props.listTitle]
+                            } </>,
+                            'user': <></>,
+                            'priority' : <>{
+                                {
+                                    0: <div className="card-tag-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 1024 1024"><path fill="currentColor" d="M112 476h160v72H112zm320 0h160v72H432zm320 0h160v72H752z"/></svg></div>,
+                                    1: <div className="card-tag-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 48 48"><g fill="currentColor"><path fill-rule="evenodd" d="M35 6a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h4a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-4Zm-1 3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v30a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V9ZM19 21a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v18a3 3 0 0 1-3 3h-4a3 3 0 0 1-3-3V21Zm3-1a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V21a1 1 0 0 0-1-1h-4Z" clip-rule="evenodd"/><path d="M6 33a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3v-6Z"/></g></svg></div>,
+                                    2: <div className="card-tag-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 48 48"><g fill="currentColor"><path d="M19 21a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v18a3 3 0 0 1-3 3h-4a3 3 0 0 1-3-3V21ZM6 33a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3v-6Z"/><path fill-rule="evenodd" d="M32 9a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v30a3 3 0 0 1-3 3h-4a3 3 0 0 1-3-3V9Zm3-1a1 1 0 0 0-1 1v30a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-4Z" clip-rule="evenodd"/></g></svg></div>,
+                                    3: <div className="card-tag-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 48 48"><path fill="currentColor" d="M32 9a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v30a3 3 0 0 1-3 3h-4a3 3 0 0 1-3-3V9ZM19 21a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v18a3 3 0 0 1-3 3h-4a3 3 0 0 1-3-3V21ZM9 30a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h4a3 3 0 0 0 3-3v-6a3 3 0 0 0-3-3H9Z"/></svg></div>,
+                                    4: <div className="card-tag-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16"><path fill="red" d="M5.96 4.457a2.075 2.075 0 1 1 4.08 0l-.856 4.56a1.205 1.205 0 0 1-2.368 0l-.855-4.56ZM9.5 12.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0Z"/></svg></div>
+                                }[props.listTitle]
+                            } </>
+                        }[props.groupValue]
+                    }
+                    
+                    <div className="list-title">
+                        {
+                            {
+                                'priority' : <>{
+                                                props.priorityList
+                                                    ? props.priorityList.map(priorityProperty => (
+                                                        priorityProperty.priority === props.listTitle
+                                                        ? <>{priorityProperty.name}</>
+                                                        : null
+                                                    ))
+                                                    : null
+                                                }</>,
+                                'status' : <>{props.listTitle}</>,
+                                'user' : <>{props.listTitle}</>
+                            }[props.groupValue]
+                        }
+                    </div>
+                    <div className="list-sum">{cardCount}</div>
+                </div>
+                <div className="list-header-right">
+                    <div className="list-add-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1z"/></svg>
+                    </div>
+                    <div className="list-option-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path fill="currentColor" d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm-7 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 3 7.8zm14 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 17 7.8z"/></svg>
+                    </div>
+                </div>
+            </div>
+
+            <div className="list-card-items">
+                {
+                    props.ticketDetails.map((ticket,index) => {
+                        if(ticket.status === props.listTitle){
+                            cardCount++;
+                            return(<Card  key={index} cardDetails={ticket} />)
+                        }
+                        else if(ticket.priority === props.listTitle){
+                            cardCount++;
+                            return(<Card  key={index} cardDetails={ticket} />)
+                        }
+                        else if(ticket.userObj.name === props.listTitle){
+                            cardCount++;
+                            return(<Card  key={index} cardDetails={ticket} />)
+                        }
+                        return null
+                    }, cardCount = 0)
+                    
+                }
+            </div>
+        </div>
+    </>
+  )
+}
